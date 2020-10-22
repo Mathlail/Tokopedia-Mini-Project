fbf66d29ff21b310cc3741960753e52d2e4b4709
Fajri Ramadhan <mathlail@femaledaily.com>
2020-10-22 17:01:05 +0700

    Initial Commit

---
+++ Tokopedia Mini Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tokopedia Mini Project
+//
+//  Created by FDN-Fajri Ramadhan on 22/10/20.
+//  Copyright © 2020 FDN-Fajri Ramadhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
